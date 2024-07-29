bab2728879e19e4e4e66b4310d0d47aea50ebd91
duykunkun <phuongduy2003.work@gmail.com>
2024-07-30 03:50:27 +0700

    update2

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className="header-contents">
                 <h2>Order your favourite food here</h2>
                 <p>
-                    Đây là dòng mô tả
+     d               Đây là dòng mô tả
                 </p>
                 <button>View Menu</button>
             </div>
